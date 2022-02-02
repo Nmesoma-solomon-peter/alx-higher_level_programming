5215c36c39470080913e4208f2b80e4b84a02a3d
nmesoma <nmesomasolo2809@gmail.com>
2022-02-02 04:08:14 +0100

    added task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-/* reads and prints the content of a file */
-
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (err, data){
-	if (err) {
- 	  return console.log(err);
- 	 } else{
-  	  console.log(data);
- 	 }
+
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  } else {
+    console.log(data);
+  }
 });
