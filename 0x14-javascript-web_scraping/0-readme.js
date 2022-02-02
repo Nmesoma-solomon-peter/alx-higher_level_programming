8188b561204ea15d893b4895070fe3fc284c2858
nmesoma <nmesomasolo2809@gmail.com>
2022-02-02 03:28:22 +0100

    added modified of task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+fs.readFile(process.argv[2], 'utf8', function (error, content){
+	if (err) {
+    console.log(err);
+  }
+  console.log(data);
 });
